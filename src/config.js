fb75d60629a28dc8d1d1ffee76a6efa9fea9d8fd
Blake <blake@kantech.com.tw>
2026-09-14 12:11:59 +0800

    降低閒置排程檢查頻率

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const taipeiOffsetHours = 8;
 export const schedulerMinimumDelayMs = 5 * 1000;
-export const schedulerIdleIntervalMs = 15 * 60 * 1000;
+export const schedulerIdleIntervalMs = 6 * 60 * 60 * 1000;
